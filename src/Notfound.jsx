b8b5ab96d331daac449f19b6c1dfad8eae208ee3
R2|Project <afrili.rizki907@gmail.com>
2025-04-29 14:24:52 +0700

    little update

--- src/Notfound.jsx
+++ src/Notfound.jsx
@@ -6,9 +6,12 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-6xl font-bold text-red-500">404</h1>
         <p className="text-xl mt-4">Oops! Page not found.</p>
-        <a href="/" className="mt-6 inline-block text-blue-500">
+        <button
+          onClick={() => window.history.back()}
+          className="mt-6 inline-block text-blue-500"
+        >
           Go back
-        </a>
+        </button>
       </div>
     </div>
   );
